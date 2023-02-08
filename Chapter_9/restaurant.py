72144cfbb6578f0f7d3f7cee6ae9b4240a8e535b
tythepopo <ttruongdc@yahoo.com>
2023-02-08 09:19:23 -0500

    Adding instance and method to calculate customer served.

--- Chapter_9/restaurant.py
+++ Chapter_9/restaurant.py
@@ -6,6 +6,7 @@ class Restaurant:
 	def __init__(self, restaurant_name, cuisine_type):
 		self.restaurant_name = restaurant_name
 		self.cuisine_type = cuisine_type
+		self.number_served = 0
 		
 	#Make a method to print out the above information
 	
@@ -17,14 +18,39 @@ class Restaurant:
 	def open_restaurant(self):
 		print(f"{self.restaurant_name} is open.")
 		
+	#Adding a method to update number of customers served.
+	
+	def increment_number_served(self, person):
+		self.number_served += person
+		return (self.number_served)
+		
 #Make an instance called restaurant.
 
 restaurant = Restaurant("cava".title(), "mexican cuisine".title())
+print(restaurant.restaurant_name, restaurant.cuisine_type)
 
 #Call the two above methods.
 
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+
+#print number of customer served using instance:
+
+print(f'The number {restaurant.restaurant_name} has served so far: {restaurant.number_served}.')
+
+#print number of customer served using assigned number to instance:
+
+restaurant.number_served = 24
+
+print(f'The number {restaurant.restaurant_name} has served so far: {restaurant.number_served}.')
+
+#print number of customer served using update method.
+
+#First, ask user input how many customer served to add on:
+
+how_many_served_today = int(input("How many customers did you have today?\n"))
+
+print(f'The number {restaurant.restaurant_name} has served so far is {restaurant.increment_number_served(how_many_served_today)}')
 
 #Make two more instances (two more restaurants).
 
