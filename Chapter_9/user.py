7edffb83cde9bf4df908188f70ac73ef0ed8621d
tythepopo <ttruongdc@yahoo.com>
2023-02-09 16:16:13 -0500

    edited users.py

--- Chapter_9/user.py
+++ Chapter_9/user.py
@@ -6,9 +6,38 @@ class User:
 		self.last_name = last_name
 		self.age = 0
 		self.location = " "
-		self.email = f'{first_name}.{last_name}@gmail.com'
+		self.email = f'{first_name.lower()}.{last_name.lower()}@gmail.com'
+		self.login_attemtp = 0
 		
 	def user_info(self):
-		return (self.first_name, self.last_name, self.age, self.email)	
-user_1 = ("jonh".title(), "nguyen".title())
-print(user_1.user_info())
+		print (f'{self.first_name}, {self.last_name}, {self.age}, {self.email}')
+
+	#Create method to inrement login attempt by 1
+
+	def increment_login_attempts(self):
+		self.login_attemtp += 1
+		return (self.login_attemtp)
+
+	#Create a method called 'reset_login_attempts' to reset the value of login
+	#attempts to 0
+
+	def reset_login_attempts(self):
+		self.login_attemtp = 0
+		return (self.login_attemtp)
+
+		
+user_1 = User("jonh".title(), "nguyen".title())
+user_1.user_info()
+
+#Checking the increment of login attempts.
+
+print(user_1.increment_login_attempts())
+print(user_1.increment_login_attempts())
+print(user_1.increment_login_attempts())
+print(user_1.increment_login_attempts())
+
+#Testing method reset_login_attempts to see if log_in_attempts reset to 0
+
+print(user_1.reset_login_attempts())
+
+
