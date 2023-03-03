a937af98137139b8011e3999b6b2b2ee857d0bb1
tythepopo <ttruongdc@yahoo.com>
2023-03-03 13:43:02 -0500

    Updating Moving both left and right

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             
             
@@ -32,8 +33,15 @@ class AlienInvasion:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_RIGHT:
                         # Move the ship to the right
-                        self.ship.rect.x += 10
-                
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+            
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
